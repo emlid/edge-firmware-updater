2a52ecc9605c2add78b99ebee91f231d0e65a51d
Ivan Smirnov <ivan.smirnov@emlid.com>
2016-05-16 17:22:15 +0300

    reworked device parameters reading

--- edge_fw_update.c
+++ edge_fw_update.c
@@ -17,6 +17,14 @@ struct scsi_block_device {
     int block;
 };
 
+void free_parameters(struct scsi_block_device dev_prop){
+    free (dev_prop.size);
+    free (dev_prop.node_path);
+    free (dev_prop.vid);
+    free (dev_prop.pid);
+    free (dev_prop.manufacturer);
+    free (dev_prop.product);
+}
 
 void print_scsi_block(struct scsi_block_device* dev) {
     printf("   VID/PID: %s/%s\n", dev->vid, dev->pid);
@@ -27,21 +35,31 @@ void print_scsi_block(struct scsi_block_device* dev) {
 }
 
 
+char* get_field_value(struct udev_device * dev, char * property_name){
+    char * field;
+    field = (char*) malloc(strlen((char*) udev_device_get_property_value(dev, property_name)));
+    strcpy(field, (char*) udev_device_get_property_value(dev, property_name));
+    return field;
+}
 
 void fill_device_param_list (struct udev_device* dev, struct scsi_block_device* dist) {
     const char* subsys;
     subsys = udev_device_get_subsystem(dev);
     if (strcmp (subsys,"scsi") == 0) {
-        dist->vid = (char*) udev_device_get_property_value(dev, "ID_VENDOR_ID");
-        dist->pid = (char*) udev_device_get_property_value(dev, "ID_MODEL_ID");
-        dist->manufacturer = (char*)udev_device_get_property_value(dev, "ID_VENDOR");
-        dist->product = (char*)udev_device_get_property_value(dev, "ID_MODEL");
+        dist->vid = get_field_value(dev, "ID_VENDOR_ID");
+        dist->pid = get_field_value(dev, "ID_MODEL_ID");
+        dist->manufacturer = get_field_value(dev, "ID_VENDOR");
+        dist->product = get_field_value(dev, "ID_MODEL");
         dist->scsi = 1;
     }
     if (strcmp (subsys,"block") == 0) {
-        dist->size = (char*)udev_device_get_sysattr_value(dev, "size");
-        dist->node_path = (char*)udev_device_get_devnode(dev);
+        dist->size = (char*) malloc(strlen((char*) udev_device_get_sysattr_value(dev, "size")));
+        strcpy( dist->size, (char*) udev_device_get_sysattr_value(dev, "size"));
+        
+        dist->node_path = (char*) malloc(strlen((char*) udev_device_get_devnode(dev)));
+        strcpy(dist->node_path, (char*) udev_device_get_devnode(dev));
         dist->block = 1;
+        
     }
 }
 
@@ -60,19 +78,17 @@ void run_rpiboot() {
 int main() {
     struct udev* udev = udev_new();
     struct udev_device* dev;
-    struct udev_device* scsi;
-    struct udev_device* usb;
-    struct udev_device* block;
     struct udev_monitor* mon;
     struct scsi_block_device dev_properties;
     int fd;
     struct timeval tv;
     int ret;
 
-    run_rpiboot();
 
     dev_properties.scsi = 0;
     dev_properties.block = 0;
+
+    run_rpiboot();
 
     mon = udev_monitor_new_from_netlink(udev, "udev");
     udev_monitor_filter_add_match_subsystem_devtype(mon, "scsi", "scsi_device");
@@ -92,18 +108,21 @@ int main() {
 
         if (ret > 0 && FD_ISSET(fd, &fds)) {
             dev = udev_monitor_receive_device(mon);
-            fill_device_param_list(dev, &dev_properties);
-            if ((dev_properties.block == 1) && (dev_properties.scsi == 1)) {
-                printf("\nDetected storage device:\n");
-                print_scsi_block(&dev_properties);
-                dev_properties.block = 0;
-                dev_properties.scsi = 0;
-                udev_device_unref(dev);
-                break;
+            if (!strcmp( udev_device_get_action(dev), "add") && !strcmp( udev_device_get_property_value(dev, "ID_VENDOR_ID"), "0a5c")){
+                fill_device_param_list(dev, &dev_properties);
+                if ((dev_properties.block == 1) && (dev_properties.scsi == 1)) {
+                    printf("\nDetected storage device:\n");
+                    print_scsi_block(&dev_properties);
+                    dev_properties.block = 0;
+                    dev_properties.scsi = 0;
+                    break;
+                }
             }
         }
     }
 
+    udev_device_unref(dev);
     udev_unref(udev);
+    free_parameters(dev_properties);    
     return 0;
 }
