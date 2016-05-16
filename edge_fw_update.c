1cd724c0b145948731d1c078bd01675c1e5e269d
Ivan Smirnov <ivan.smirnov@emlid.com>
2016-05-16 14:24:25 +0300

    removed unnecessary code
    - enumerate_usb_mass_storage() is redundant if Compute_Module have to be detected every time by rpiboot
    - added GB size outputting

--- edge_fw_update.c
+++ edge_fw_update.c
@@ -22,8 +22,8 @@ void print_scsi_block(struct scsi_block_device* dev) {
     printf("   VID/PID: %s/%s\n", dev->vid, dev->pid);
     printf("   Manufacturer: %s\n", dev->manufacturer);
     printf("   Product: %s\n", dev->product);
-    printf("   Size: %s blocks\n", dev->size);
-    printf("   Node path: %s\n", dev->node_path);
+    printf("   Size: %s blocks (%f GB)\n", dev->size, atof(dev->size) * 512 / 1e9);
+    printf("   Node path: %s\n\n", dev->node_path);
 }
 
 
@@ -43,88 +43,9 @@ void fill_device_param_list (struct udev_device* dev, struct scsi_block_device* 
         dist->node_path = (char*)udev_device_get_devnode(dev);
         dist->block = 1;
     }
-    /* this part moved to main while loop
-    if ((dist->block == 1) && (dist->scsi == 1)) {
-        printf("\nAction: %s\n", udev_device_get_action(dev));
-        print_scsi_block(dist);
-        dist->block = 0;
-        dist->scsi = 0;
-        udev_device_unref(dev);
-    }*/ 
 }
 
 
-
-
-static struct udev_device* get_child(struct udev* udev, struct udev_device* parent, const char* subsystem) {
-    struct udev_device* child = NULL;
-    struct udev_enumerate *enumerate;
-    struct udev_list_entry *devices;
-    struct udev_list_entry *entry;
-    const char *path;
-
-    enumerate = udev_enumerate_new(udev);
-    udev_enumerate_add_match_parent(enumerate, parent);
-    udev_enumerate_add_match_subsystem(enumerate, subsystem);
-    udev_enumerate_scan_devices(enumerate);
-
-    devices = udev_enumerate_get_list_entry(enumerate);
-
-    udev_list_entry_foreach(entry, devices) {
-        path = udev_list_entry_get_name(entry);
-        child = udev_device_new_from_syspath(udev, path);
-        break;
-    }
-
-    udev_enumerate_unref(enumerate);
-    return child;
-}
-
-static void enumerate_usb_mass_storage(struct udev* udev) {
-    struct udev_enumerate* enumerate;
-    struct udev_list_entry *devices;
-    struct udev_list_entry *entry;
-    const char* path;
-    struct udev_device* scsi;
-    struct udev_device* block;
-    struct udev_device* usb;
-
-    enumerate = udev_enumerate_new(udev);
-
-    udev_enumerate_add_match_subsystem(enumerate, "scsi");
-    udev_enumerate_add_match_property(enumerate, "DEVTYPE", "scsi_device");
-
-    udev_enumerate_scan_devices(enumerate);
-
-    devices = udev_enumerate_get_list_entry(enumerate);
-
-    udev_list_entry_foreach(entry, devices) {
-        path = udev_list_entry_get_name(entry);
-        scsi = udev_device_new_from_syspath(udev, path);
-
-        block = get_child(udev, scsi, "block");
-
-        usb = udev_device_get_parent_with_subsystem_devtype(scsi, "usb", "usb_device");
-
-        if (block && usb) {
-            printf("Device Node Path = %s\n", udev_device_get_devnode(block));
-            printf("  VID/PID: %s/%s\n",
-                    udev_device_get_property_value(usb, "ID_VENDOR_ID"),
-                    udev_device_get_property_value(usb, "ID_MODEL_ID"));
-            printf("  Manufacturer:  %s\n", udev_device_get_property_value(usb, "ID_VENDOR"));
-            printf("  Product: %s\n", udev_device_get_property_value(usb,"ID_MODEL"));
-            printf("  Size: %s blocks\n", udev_device_get_sysattr_value(block, "size"));
-        }
-
-        if (block) {
-            udev_device_unref(block);
-        }
-
-        udev_device_unref(scsi);
-    }
-
-    udev_enumerate_unref(enumerate);
-}
 
 void run_rpiboot() {
     int ret =  system("sudo rpiboot");
@@ -159,8 +80,6 @@ int main() {
     udev_monitor_enable_receiving(mon);
     fd = udev_monitor_get_fd(mon);
 
-    enumerate_usb_mass_storage(udev);
-   
     while (1) {
         fd_set fds;
 
