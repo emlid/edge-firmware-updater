6b990543cc3ff0040b33c28310e7b7bf95cadd4e
Vladimir ProvaLove <vprovalov@yandex.ru>
2017-10-18 19:34:35 +0300

    rpiboot/rpiboot.cpp: added vid, pid defines.

--- rpiboot/rpiboot.cpp
+++ rpiboot/rpiboot.cpp
@@ -9,6 +9,13 @@
 
 #include "rpiboot.h"
 
+
+#define VENDOR_ID       0xa5c
+#define VENDOR_ID_CHAR   "0a5c"
+#define PRODUCT_ID1     0x2763
+#define PRODUCT_ID2     0x2764
+
+
 using namespace std;
 
 int verbose = 0;
