a0c854657fa1df0c0bbb70ddfc848561ac90c820
Vladimir ProvaLove <vprovalov@yandex.ru>
2017-10-26 21:09:33 +0300

    testnotifier.cpp: refactored

--- testnotifier.cpp
+++ testnotifier.cpp
@@ -5,5 +5,5 @@ TestNotifier::TestNotifier(Flasher const& flasher, QObject *parent) : QObject(pa
     connect(&flasher, &Flasher::flashStarted, this, &TestNotifier::onFlashStarted);
     connect(&flasher, &Flasher::flashCompleted, this, &TestNotifier::onFlashCompleted);
     connect(&flasher, &Flasher::progressChanged, this, &TestNotifier::onProgressChanged);
-    connect(&flasher, &Flasher::flashAborted, this, &TestNotifier::onFlashAborted);
+    connect(&flasher, &Flasher::flashFailed, this, &TestNotifier::onFlashAborted);
 }
