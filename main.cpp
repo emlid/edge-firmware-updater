2f9c89a3be41a204e65b26e06e34d63fd41824fb
Vladimir ProvaLove <vprovalov@yandex.ru>
2017-10-26 21:08:06 +0300

    main.cpp: added remoting

--- main.cpp
+++ main.cpp
@@ -1,50 +1,20 @@
-#include <QCoreApplication>
+#include <QtRemoteObjects>
 #include <QtCore>
-#include <iostream>
 
-#include "testnotifier.h"
-#include "Flasher.h"
-#include "StorageDeviceManager.h"
-#include "rpiboot/rpiboot.h"
-
+#include "RemoteFlasherWatcher.h"
 
 int main(int argc, char *argv[])
 {
-    qSetMessagePattern("%{type} : %{message}");
-
     QCoreApplication a(argc, argv);
 
-    auto manager = StorageDeviceManager::instance();
+    qSetMessagePattern("%{type} : %{message}");
 
-    auto physicalDrives = manager->physicalDrives(0x0a5c, 0x2764);
+    auto serverUrl = QUrl(QStringLiteral("local:edge_fw_upgrader"));
+    QRemoteObjectHost serverNode(serverUrl);
 
-    qInfo() << "devices count: " << physicalDrives.size();
-
-    for (std::shared_ptr<StorageDevice> device : physicalDrives) {
-        QTextStream(stdout) << device->toString();
-
-        /*device->unmountAllMountpoints();
-
-        QFile src("/home/vladimir.provalov/Downloads/emlid-raspbian-20170323.img");
-        QFile dest;
-
-        if (device->openAsQFile(&dest).failed()) {
-            qCritical() << "Failed to open as qfile.";
-            std::exit(1);
-        }
-
-        if (!src.open(QIODevice::ReadOnly)) {
-            qCritical() << "Failed to open image.";
-            std::exit(1);
-        }
-
-        Flasher flasher;
-        TestNotifier notif(flasher);
-
-        if (!flasher.flash(src, dest)) {
-            QTextStream("failed");
-        }*/
-    }
+    // Remote our watcher to other processes
+    RemoteFlasherWatcher watcher;
+    serverNode.enableRemoting(&watcher);
 
     return a.exec();
 }
