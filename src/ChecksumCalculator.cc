3569abc70a743a9508d23408e5686051b3a820b8
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2017-11-05 22:49:03 +0300

    ChecksumCalculator.*: added checksum calculator

---
+++ src/ChecksumCalculator.cc
@@ -0,0 +1,54 @@
+#include "ChecksumCalculator.h"
+
+ChecksumCalculator::ChecksumCalculator(QCryptographicHash::Algorithm algo)
+    : _hash(algo)
+{  }
+
+
+QByteArray ChecksumCalculator::calculate(QFile &file, qint64 length, int ioBlockSize)
+{
+    if (!file.isReadable()) {
+        qWarning() << "File not readeble";
+        emit errorOcurred();
+        return QByteArray();
+    }
+
+    file.seek(0);
+
+    auto fileSize = length;
+    auto progress = 0;
+
+    auto readed = 0l;
+
+    while (readed < fileSize) {
+        if (readed + ioBlockSize > fileSize) {
+            ioBlockSize = fileSize - readed;
+        }
+
+        auto data = file.read(ioBlockSize);
+        _hash.addData(data);
+
+        readed += data.size();
+
+        if (readed == 0) {
+            break;
+        }
+
+        auto currProgress = static_cast<int>((readed * 100) / fileSize);
+        if (currProgress != progress) {
+            progress = currProgress;
+            qInfo() << "Progress " << progress << "%";
+            emit progressChanged(progress);
+        }
+    }
+
+    qInfo() << "File size: " << fileSize;
+    qInfo() << "Readed size: " << readed;
+
+    auto checksum = _hash.result();
+
+    file.seek(0);
+    _hash.reset();
+
+    return checksum;
+}
---
+++ src/ChecksumCalculator.h
@@ -0,0 +1,23 @@
+#ifndef CHECKSUMCALCULATOR_H
+#define CHECKSUMCALCULATOR_H
+
+#include <QtCore>
+
+class ChecksumCalculator : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ChecksumCalculator(QCryptographicHash::Algorithm algo =
+            QCryptographicHash::Algorithm::Md5);
+
+    QByteArray calculate(QFile& file, qint64 length, int ioBlockSize = 4096);
+
+signals:
+    void progressChanged(int value);
+    void errorOcurred();
+
+private:
+    QCryptographicHash _hash;
+};
+
+#endif // CHECKSUMCALCULATOR_H
