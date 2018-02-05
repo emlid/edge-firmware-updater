52b8fe73b6b09705eb56ad30cc986477aeddb985
Vladimir ProvaLove <vprovalov@yandex.ru>
2018-02-05 16:48:18 +0300

    Subtasks: add open and close image file - for Checksum and Flasher

--- src/FlasherSubtask.cpp
+++ src/FlasherSubtask.cpp
@@ -19,6 +19,24 @@ void FlasherSubtask::run(void)
     util::Stopwatch stopwatch;
     Flasher         flasher;
 
+    // Try to open file with Image
+    auto succeed = _src->open(QIODevice::ReadOnly);
+
+    if (!succeed) {
+        sendLogMessage(": can not open image file", Error);
+        emit finished(Failed);
+        return;
+    }
+
+    // Try to open file, which represent rpi device in filesystem
+    succeed = _dest->open(QIODevice::ReadWrite | QIODevice::Unbuffered);
+
+    if (!succeed) {
+        emit sendLogMessage("can not open device", Error);
+        emit finished(AbstractSubtask::Failed);
+        return;
+    }
+
     stopwatch.start();
 
     connect(&flasher, &Flasher::flashFailed,
@@ -42,6 +60,7 @@ void FlasherSubtask::run(void)
     auto ioBlockSize = 1 << 16; // 64kb
     auto successful = flasher.flash(_src, _dest, ioBlockSize);
 
+    _src->close();
     if (!successful) {
         sendLogMessage("flashing failed", Error);
         emit finished(Failed);
