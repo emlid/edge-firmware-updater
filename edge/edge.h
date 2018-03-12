784d13e4a8cf84eed852baf3db894f73b27869dc
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-03-12 17:57:20 +0300

    edge: add edge.h - header with all interfaces

---
+++ edge/edge.h
@@ -0,0 +1,16 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+#include <QString>
+#include <QIODevice>
+
+#include "EdgeConfig.h"
+#include "EdgeDevice.h"
+#include "EdgeManager.h"
+
+namespace edge {
+    auto makeEdgeManager(edge::EdgeConfig const& config)
+        -> std::unique_ptr<edge::IEdgeManager>;
+}
+
+#endif // EDGE_H
