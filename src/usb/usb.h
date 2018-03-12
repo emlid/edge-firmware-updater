367b22e86ed4e94084fe6a1c42abeb8da45b795c
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-03-12 17:27:33 +0300

    purge old source code

--- src/usb/usb.h
+++
@@ -1,7 +0,0 @@
-#ifndef USB_H
-#define USB_H
-
-#include "rpiboot.h"
-#include "usbutils.h"
-
-#endif // USB_H
--- src/util/util.pri
+++
@@ -1,5 +0,0 @@
-HEADERS += $$PWD/util.h \
-    $$PWD/Stopwatch.h
-
-SOURCES += \
-    $$PWD/Stopwatch.cpp
