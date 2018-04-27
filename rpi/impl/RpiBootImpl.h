86476976f38fc55347df72ddd63fab2cb43e0480
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-04-27 13:25:26 +0300

    rpi: improve logging
    - add signals for messages
    - use _sendMessage for important log messages

--- rpi/impl/RpiBootImpl.h
+++ rpi/impl/RpiBootImpl.h
@@ -14,11 +14,13 @@ class RpiBootPrivate;
 
 class usb::RpiBootImpl : public usb::IRpiBoot
 {
+    Q_OBJECT
 public:
-    RpiBootImpl(int vid, QList<int> const& pid);
-    virtual ~RpiBootImpl(void);
+    RpiBootImpl(int vid, QList<int> const& pid, QObject* parent = nullptr);
+    ~RpiBootImpl(void) override;
+
 private:
-    virtual int bootAsMassStorage_core(void) override;
+    int bootAsMassStorage_core(void) override;
     std::unique_ptr<RpiBootPrivate> _pimpl;
 };
 
